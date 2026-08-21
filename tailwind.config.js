68009aae2ddd202cfdf877a52a427cc7b2b75374
Ankit Jangir <ankitjangir529@gmail.com>
2026-08-21 18:48:51 +0530

    fix: Expand Tailwind content scope and include forms & container query rules in compiled output.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,9 @@ export default {
   darkMode: "class",
   content: [
     "./*.html",
+    "./*.js",
     "./public/*.html",
+    "./public/*.js",
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
